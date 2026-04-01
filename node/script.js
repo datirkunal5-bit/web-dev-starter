f3e7969acc470143bed7fe0551c74e838420df05
Kunal Datir <datirkunal5@gmail.com>
2026-04-01 19:45:57 +0530

    Insatlling package (use figlet)

--- node/script.js
+++ node/script.js
@@ -14,3 +14,17 @@ for(i=0;i<p.length;i++)
 {
     console.log("hello",p[i]);
 }
+
+module.exports = 123;
+
+
+const figlet = require('figlet');
+
+figlet("Hello Kunal  !!", function (err, data) {
+  if (err) {
+    console.log("Something went wrong...");
+    console.dir(err);
+    return;
+  }
+  console.log(data);
+});
